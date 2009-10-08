08f6df9a899dd6bd89ee2344744e6e7ca48749f8
Shixin Zeng <zeng.shixin@gmail.com>
2009-10-08 00:41:47 -0500

    avoid relative including path

--- gdk-pixbuf/gdk-pixbuf-csource.c
+++ gdk-pixbuf/gdk-pixbuf-csource.c
@@ -19,7 +19,7 @@
 #include "config.h"
 
 #define __GTK_H_INSIDE__
-#include "../gtk/gtkversion.h"	/* versioning */
+#include "gtkversion.h"	/* versioning */
 #undef __GTK_H_INSIDE__
 #include "gdk-pixbuf.h"
 #include "gdk-pixdata.h"
